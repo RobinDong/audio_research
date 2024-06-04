62bb69b00f47145b3e9386b2624b693b0c9ad07d
Robin Dong <robin.k.dong@gmail.com>
2024-06-04 16:24:42 +1000

    Add tool to convert flac files of AudioSet to flat numpy format

---
+++ utils/export_audio_set.py
@@ -0,0 +1,45 @@
+import os
+import fire
+import glob
+import librosa
+import numpy as np
+
+from pathlib import Path
+from tqdm import tqdm
+
+NR_MELS = 128
+FMIN = 20
+FMAX = 20000
+QUANT_RANGE = 4  # range (-4, 4)
+QUANT_GRANU = 256  # 8bit
+
+
+class AudioSetExporter:
+    def export(self, audio_dir: str, npy_dir: str):
+        file_lst = glob.glob(f"{audio_dir}/*/*.flac")
+        if not os.path.exists(npy_dir):
+            os.mkdir(npy_dir)
+        for filename in tqdm(file_lst):
+            data, sr = librosa.load(filename)
+            # We use 20Hz to 20kHz as the range of human hearing
+            spectro = librosa.feature.melspectrogram(
+                y=data, sr=sr, n_mels=NR_MELS, fmin=FMIN, fmax=FMAX
+            )
+            s_db = librosa.power_to_db(spectro, ref=np.max)
+
+            means = s_db.mean()
+            stds = s_db.std()
+            normalized = (s_db - means) / stds
+            # range (-4, 4)
+            normalized = (
+                (normalized + QUANT_RANGE) * QUANT_GRANU * 2 / QUANT_RANGE
+            ).astype(np.uint8)
+
+            new_name = Path(filename).stem + ".npy"
+            with open(f"{npy_dir}/{new_name}", "wb") as fp:
+                np.save(fp, normalized)
+
+
+if __name__ == "__main__":
+    exporter = AudioSetExporter()
+    fire.Fire(exporter.export)
