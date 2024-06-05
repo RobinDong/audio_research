b496bce0209b74c43dbc4df0776707b12d812dbb
Robin Dong <robin.k.dong@gmail.com>
2024-06-05 15:36:25 +1000

    Add ESC-50 dataset (https://github.com/karolpiczak/ESC-50\?tab\=readme-ov-file)

---
+++ datasets/esc50.py
@@ -0,0 +1,59 @@
+import glob
+import numpy as np
+
+from pathlib import Path
+from torch.utils.data import Dataset
+
+FRAMES_PER_SEC = 62.6
+FRAMES_WIN = int(FRAMES_PER_SEC * 2)  # 2 seconds
+
+
+class AudioSetDataset(Dataset):
+    def __init__(self, npy_dir: str, meta_dir: str, validation: bool = False):
+        self.file_lst = glob.glob(f"{npy_dir}/*.npy")
+        self.validation = validation
+        self.category_map = self.load_meta(meta_dir)
+
+    def load_meta(self, meta_dir: str) -> dict[str, int]:
+        category_map = {}
+        csv_file = f"{meta_dir}/esc50.csv"
+        with open(csv_file, "r") as fp:
+            lines = fp.readlines()
+            for line in lines[1:]:  # skip first line (title)
+                arr = line.split(",")
+                filename, label = arr[0], int(arr[2])
+                category_map[Path(filename).stem] = label
+        return category_map
+
+    def __len__(self):
+        return len(self.file_lst)
+
+    def __getitem__(self, index):
+        filename = self.file_lst[index]
+        try:
+            sound = np.load(filename, mmap_mode="r")
+        except Exception as ex:
+            print(ex)
+            print(f"Failed to load {filename}")
+
+        len_sound = len(sound)
+        assert len_sound >= FRAMES_WIN
+
+        if self.validation:
+            start = 0
+        else:
+            start = np.random.randint(len_sound - FRAMES_WIN)
+
+        sound = sound[:, start : start + FRAMES_WIN].astype(np.float16)
+        return sound, self.category_map[Path(filename).stem]
+
+
+if __name__ == "__main__":
+    ds = AudioSetDataset(
+        "/Users/robin/Downloads/ESC-50-master/npy",
+        "/Users/robin/Downloads/ESC-50-master/meta/",
+    )
+    sound, label = ds[1023]
+    print(sound.shape, label)
+    sound, label = ds[123]
+    print(sound.shape, label)
