ac86dcb371585b1625a5dca575bc6c4e209893b6
Robin Dong <robin.k.dong@gmail.com>
2024-06-17 20:49:29 +1000

    After reducing batch_size, it could get 0.408 val accuracy in about 4 minutes

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ class TrainConfig:
     eval_ratio: float = 0.01
     num_workers: int = 4
     lr: float = 1e-2
-    batch_size: int = 64
-    min_lr: float = 5e-3
+    batch_size: int = 16
+    min_lr: float = 6e-3
     grad_clip: float = 10.0
     log_iters: int = 100
     eval_iters: int = 500
     warmup_iters: int = 2 * eval_iters
-    lr_decay_iters: int = 6000
+    lr_decay_iters: int = 4000
     max_iters: int = lr_decay_iters
     num_classes: int = 50
     dataset_name: str = "ESC-50"
